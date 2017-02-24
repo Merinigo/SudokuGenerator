2b9eb4d9028c46b766ac4128c85e1e40654fe93c
Iñigo <merinigo@gmail.com>
2017-02-24 09:10:34 +0100

    Nombre paquete cambiado

--- src/com/merinigo/sudokugenerator/Sudoku.java
+++ src/com/merinigo/sudokugenerator/Sudoku.java
@@ -1,4 +1,4 @@
-package main;
+package com.merinigo.sudokugenerator;
 
 public class Sudoku {
 
--- src/com/merinigo/sudokugenerator/SudokuGenerator.java
+++ src/com/merinigo/sudokugenerator/SudokuGenerator.java
@@ -1,4 +1,4 @@
-package main;
+package com.merinigo.sudokugenerator;
 
 import java.util.ArrayList;
 import java.util.Arrays;
